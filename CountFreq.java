b72d6c81c558c2b161b5bb68a63b8975ae605c6c
Rahul <rahul@alphanso-2.local>
2021-01-20 22:09:06 +0530

    Count frequencies of all elements in array in O(1) extra space and O(n) time

---
+++ CountFreq.java
@@ -0,0 +1,38 @@
+public class CountFreq {
+	public static void main(String[] args) {
+		int[] arr = new int[args.length];
+		for(int i = 0; i< args.length; i++) {
+			arr[i] = Integer.valueOf(args[i]);
+		}
+		count(arr);
+	}
+
+	public static void count(int[] arr) {
+		int i = 0;
+		while(i < arr.length) {
+			if(arr[i] <= 0) {
+				i++;
+			} else {
+				int a = arr[i];
+			    int b = arr[a-1];
+			 	
+				if(arr[a-1] < 0) {
+					arr[i] = 0;
+					arr[a-1] = arr[a-1] -1;
+					i++;
+				} else {
+					if(arr[i] == arr[a-1] && i != a-1) {
+						arr[i] = 0;
+						arr[a-1] = -2;
+					} else {
+						arr[i] = b;
+						arr[a-1] = -1;
+					}
+				}			
+			}
+		}
+		for (int w = 0; w < arr.length; w++) {
+			System.out.printf("%s->%s%n", w+1, Math.abs(arr[w]));
+		}
+	}
+}
